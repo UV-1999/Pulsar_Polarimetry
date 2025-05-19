bc3d1ac2bb21af8d4cd7a9a6438aece7fa0fa845
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2025-05-19 16:55:28 +0200

    Update plot_polarisation_distribution.py

--- modules/plot_polarisation_distribution.py
+++ modules/plot_polarisation_distribution.py
@@ -1,4 +1,3 @@
-
 def plot_polarization_histograms(data, fraction=0.1, quantity_bins=200):
     """
     Plots 2D histograms of PA, EA, I, P/I, L/I, and V/I vs phase.
@@ -8,27 +7,27 @@ def plot_polarization_histograms(data, fraction=0.1, quantity_bins=200):
     phase_axis = np.linspace(0, 1, num_phase_bins)
 
     # Extract Stokes parameters (intergrated over all pulse, intensity as a function of phase)
-    I = data[:, 0, :].mean(axis=0)
-    Q = data[:, 1, :].mean(axis=0)
-    U = data[:, 2, :].mean(axis=0)
-    V = data[:, 3, :].mean(axis=0)
+    I = data[:, 0, :]
+    Q = data[:, 1, :]
+    U = data[:, 2, :]
+    V = data[:, 3, :]
   
     # Define on-pulse as where intensity >= fraction * max
-    threshold = fraction * np.max(I)
+    threshold = fraction * np.max(I.mean(axis=0))
     on_pulse_mask = I >= threshold
     off_pulse_mask = ~on_pulse_mask
     on_pulse_indices = np.where(on_pulse_mask)[0]
     off_pulse_indices = np.where(off_pulse_mask)[0]
 
     # Calculate off-pulse standard deviation
-    off_pulse_std = np.std(intensity[off_pulse_mask])
+    off_pulse_std = np.std(I[off_pulse_mask])
 
     # Derived quantities
     L = np.sqrt(Q**2 + U**2)
     L_true = np.zeros_like(L)
-    L_sigma = L / sigma_off
+    L_sigma = L /  off_pulse_std
     mask = L_sigma >= 1.57
-    L_true[mask] = sigma_off * np.sqrt(L_sigma[mask]**2 - 1)
+    L_true[mask] =  off_pulse_std * np.sqrt(L_sigma[mask]**2 - 1)
     p_frac = np.where(I != 0, np.sqrt(Q**2 + U**2 + V**2) / I, 0)
     l_frac = np.where(I != 0, L_true / I, 0)
     v_frac = np.where(I != 0, V / I, 0)
@@ -40,14 +39,15 @@ def plot_polarization_histograms(data, fraction=0.1, quantity_bins=200):
     labels = [
         "Polarization Angle (PA) [rad]",
         "Bias Corrected Ellipticity Angle (EA) [rad]",
-        "Total Intensity (I)", "polarised fraction (p)"
+        "Total Intensity (I)", "polarised fraction (p)",
         "Bias Corrected Linear Polarization Fraction (L/I)",
         "Circular Polarization Fraction (V/I), Absolute Circular Polarization Fraction (|V|/I)"
     ]
 
     fig, axs = plt.subplots(4, 2, sharex=True, constrained_layout=True)
-
-    for ax, quantity, label in zip(axs, quantities, labels):
+    axs = axs.flatten()
+    
+    for ax, quantity, label in zip(axs[:len(quantities)], quantities, labels):
         # Shape: (phase_bin, samples)
         q = quantity.T
 
